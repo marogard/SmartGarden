8e420cdc4800ded57aa0d08c6a75ac579ce3ed04
marogard <83023563+marogard@users.noreply.github.com>
2021-04-23 02:24:51 +0200

    Add files via upload

--- Radiator_Working.py
+++ Radiator_Working.py
@@ -1,7 +1,14 @@
 import requests
 import time
 import adafruit_dht
+import logging
+from datetime import datetime
 from board import *
+
+#Logging library not working here, cannot be installed / gives errors.
+#log = logging.getLogger("my-logger")
+#logging.basicConfig(filename=/home/pi/Dokumente/Logs/RadiatorLog.log, encoding='utf-8',level=logging.DEBUG)
+#log.info("Hello, world")
 
 #Sensor Pin for readout of Data from DHT22 sensor
 SENSOR_PIN = D4
@@ -17,12 +24,14 @@ payload_off = {"user": "admin", "password": "KahlanAmnell!1", "cmnd": "Power OFF
 payload_toggle = {"user": "admin", "password": "KahlanAmnell!1", "cmnd": "Power TOGGLE"}
 payload_status = {"user": "admin", "password": "KahlanAmnell!1", "cmnd": "Power"}
 
+#Timestamps for log and printing
+time_formatted = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
 #Temperature & humidity request retry functions THANKS TO MAXIMILIAN P.
 def getTemperature(countTries=5):
     for x in range(0, countTries):
         try:
-            temperature = dht22.temperature
-            return temperature;
+            return dht22.temperature;
         except RuntimeError:
             print("Failed to retrieve temperature. Trying again.")
     return 0
@@ -35,6 +44,14 @@ def getHumidity(countTries=5):
         except RuntimeError:
             print("Failed to retrieve humidity. Trying again.")
     return 0
+
+#Logging function to specified file
+def log(text):
+    f = open("/home/pi/Dokumente/Logs/RadiatorLog.txt", "a")
+    f.write(time_formatted + ": " + text)
+    f.close()
+    print(time_formatted + ": " + text)
+
 
 #Target temperature & humidity variables
 targetTempUpperBound = 23
@@ -57,7 +74,7 @@ targetHum = 60
 
 temperature = getTemperature()
 humidity = getHumidity()
-print("Starting humidity is "+ str(humidity) + "%, temperature is " + str(temperature) + "°C.")
+logging.info("Starting humidity is "+ str(humidity) + "%, temperature is " + str(temperature) + "°C.")
 
 
 #Now actual loop for temperature management
@@ -66,27 +83,27 @@ while True:
     temperature = getTemperature()
     if float(temperature) < float(targetTempLowerBound) and "OFF" in r.text :
         r=requests.get("http://192.168.0.87/cm", params= payload_on)
-        print("Beginning heating sequence.")
+        log("Beginning heating sequence.")
         time.sleep(20)
     else:
         if float(temperature) < float(targetTempUpperBound):
-            print(str(temperature) + "°C, still heating up to " + str(targetTempUpperBound) + "°C, power is on.")
+            log(str(temperature) + "°C, still heating up to " + str(targetTempUpperBound) + "°C, power is on.")
             time.sleep(30)
         else:
             r=requests.get("http://192.168.0.87/cm", params= payload_off)
-            print("Target temperature of " + str(targetTempUpperBound) + "°C exceeded, current temperature is " + str(temperature) +"°C, shutting down.")
+            log("Target temperature of " + str(targetTempUpperBound) + "°C exceeded, current temperature is " + str(temperature) +"°C, shutting down.")
             r=requests.get("http://192.168.0.87/cm", params= payload_status)
             if "OFF" in r.text:
-                print("Shutdown successful, timing out for 10 minutes.")
+                log("Shutdown successful, timing out for 10 minutes.")
                 time.sleep(600)
             else:
                 r=requests.get("http://192.168.0.87/cm", params= payload_off)
                 time.sleep(5)
                 r=requests.get("http://192.168.0.87/cm", params= payload_status)
                 if "OFF" in r.text:
-                    print("Shutdown successful, timing out for 10 minutes.")
+                    log("Shutdown successful, timing out for 10 minutes.")
                     time.sleep(600)
                 else:
-                    print("Problem with shutdown, please disconnect manually. Automatic retry in 20 seconds.")
+                    log("Problem with shutdown, please disconnect manually. Automatic retry in 20 seconds.")
                     time.sleep(20)
             
